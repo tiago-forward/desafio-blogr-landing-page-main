3e0567576fd98cacc844074945ce93c5d9f3fc7f
tiagoforward <devtiagoforward@gmail.com>
2024-03-16 16:26:47 -0300

    Some adjustments in the sections

--- src/components/Section/IllustrativeContentSection/index.jsx
+++ src/components/Section/IllustrativeContentSection/index.jsx
@@ -2,19 +2,26 @@ import styled from "styled-components"
 
 const IllustrativeContentSection = ({ image, titleMain, paragraph }) => {
     return (
-        <>
+        <Pai>
+            <Content>
+                <Image src={image} alt={image} />
+            </Content>
             <Container>
-            <Image src={image} alt={image} />
-                <Content>
-                    <TitleMain>{titleMain}</TitleMain>
-                    <Paragraph>{paragraph}</Paragraph>
-                </Content>
+                <TitleMain>{titleMain}</TitleMain>
+                <Paragraph>{paragraph}</Paragraph>
             </Container>
-        </>
+        </Pai>
     )
 }
 
 export { IllustrativeContentSection }
+
+const Pai = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const Container = styled.div`
     width: 100%;
@@ -25,21 +32,25 @@ const Container = styled.div`
     border-bottom-left-radius: 100px;
     padding-bottom: 100px;
     margin-top: 250px;
+    z-index: 0;
 `
 
 const Content = styled.div`
     background-image: url("../../../../public/images/bg-pattern-circles.svg");
     background-repeat: no-repeat;
     background-size: cover;
-    background-position-y: -140px;
-    background-position-x: -73px;
-    position: relative;
+    background-position-y: -70px;
+    background-position-x: 0px;
+    position: absolute;
+    z-index: 1;
 `
 
 const Image = styled.img`
-    position: absolute;
-    width: 100%;
-    bottom: -148%;
+    display: block;
+    margin: 0 auto;
+    position: relative;
+    width: 340px;
+    top: -150px;
     z-index: 1;
 `
 
@@ -47,7 +58,7 @@ const TitleMain = styled.h2`
     color: var(--white);
     text-align: center;
     font-size: 30px;
-    padding: 200px 30px 40px 30px;
+    padding: 210px 30px 40px 30px;
 `
 
 const Paragraph = styled.p`
