b2a93c8f746383e012bb6ffa9fccef47f905c415
tiagoforward <devtiagoforward@gmail.com>
2024-03-18 13:38:57 -0300

    Finished section footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,32 +1,41 @@
 import styled from "styled-components"
+import Logo from "../../../public/images/logo.svg"
 
 const Footer = () => {
     return (
         <>
             <FooterContainer>
-                <nav>
-                    <ul>
-                        <li>Product</li>
-                        <li>Overview</li>
-                        <li>Pricing</li>
-                        <li>Marketplace</li>
-                        <li>Features</li>
-                        <li>Integrations</li>
-                    </ul>
-                    <ul>
-                        <li>Company</li>
-                        <li>About</li>
-                        <li>Team</li>
-                        <li>Blog</li>
-                        <li>Careers</li>
-                    </ul>
-                    <ul>
-                        <li>Connect</li>
-                        <li>Contact</li>
-                        <li>Newsletter</li>
-                        <li>Linkedin</li>
-                    </ul>
-                </nav>
+                <Nav>
+
+                    <ImageLogo src={Logo} alt="Logo Blogr" />
+                    <div>
+                        <Span>Product</Span>
+                        <NavList>
+                            <NavLink><Link href="/">Overview</Link></NavLink>
+                            <NavLink><Link href="/">Pricing</Link></NavLink>
+                            <NavLink><Link href="/">Marketplace</Link></NavLink>
+                            <NavLink><Link href="/">Features</Link></NavLink>
+                            <NavLink><Link href="/">Integrations</Link></NavLink>
+                        </NavList>
+                    </div>
+                    <div>
+                        <Span>Company</Span>
+                        <NavList>
+                            <NavLink><Link href="/">About</Link></NavLink>
+                            <NavLink><Link href="/">Team</Link></NavLink>
+                            <NavLink><Link href="/">Blog</Link></NavLink>
+                            <NavLink><Link href="/">Careers</Link></NavLink>
+                        </NavList>
+                    </div>
+                    <div>
+                        <Span>Connect</Span>
+                        <NavList>
+                            <NavLink><Link href="/">Contact</Link></NavLink>
+                            <NavLink><Link href="/">Newsletter</Link></NavLink>
+                            <NavLink><Link href="/">Linkedin</Link></NavLink>
+                        </NavList>
+                    </div>
+                </Nav>
             </FooterContainer>
         </>
     )
@@ -40,4 +49,40 @@ const FooterContainer = styled.footer`
     border-top-right-radius: 100px;
     margin-top: 100px;
     text-align: center;
+`
+
+const ImageLogo = styled.img`
+    margin-bottom: 50px;
+`
+
+const Nav = styled.nav`
+    height: 100%;
+    padding: 50px 20px;
+
+    @media screen and (min-width: 1024px) {
+        display: flex;
+        align-items: start;
+        justify-content: space-around;
+    }
+`
+
+const Span = styled.span`
+    font-size: 18px;
+`
+
+const NavList = styled.ul`
+    margin: 30px 0px;
+`
+
+const NavLink = styled.li`
+    padding: 3px;
+`
+
+const Link = styled.a`
+    color: var(--grayish-blue);
+    font-size: 15px;
+    
+    &:hover {
+        text-decoration: 2px underline;
+    }
 `
